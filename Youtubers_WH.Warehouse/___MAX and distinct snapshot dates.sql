ccaffa646fb612f40595c2b245c90173e9492f79
Abdulmaleek Mubaraq <mubaraqabdulmaleek@gmail.com>
2025-10-05 23:00:54 +0100

    style(sql): add alias to snapshot date in distinct selection, and added architecture diagram

--- Youtubers_WH.Warehouse/___MAX and distinct snapshot dates.sql
+++ Youtubers_WH.Warehouse/___MAX and distinct snapshot dates.sql
@@ -1,4 +1,4 @@
 ------ MAX and distinct snapshot dates
 SELECT max([Snapshot Date]), COUNT(DISTINCT [Snapshot Date]) FROM [Youtubers_WH].[dbo].[channelSnapshot_gold];
 
-SELECT DISTINCT [Snapshot Date] FROM [Youtubers_WH].[dbo].[channelSnapshot_gold] ORDER BY 1 desc
+SELECT DISTINCT [Snapshot Date] AS [Snapshot Dates] FROM [Youtubers_WH].[dbo].[channelSnapshot_gold] ORDER BY 1 DESC
